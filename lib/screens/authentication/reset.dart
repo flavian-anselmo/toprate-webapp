556fded9bde89714af04369939d20caf9a44b86a
flavian-anselmo <anselmoflavian4@gmail.com>
2022-01-12 23:36:38 +0300

    fixed the UI ---MODULES

--- lib/screens/authentication/reset.dart
+++ lib/screens/authentication/reset.dart
@@ -20,64 +20,85 @@ class _ResetForgotenPasswordState extends State<ResetForgotenPassword> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Form(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const AnimatedTxt(),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: ktextFieldDEcoratiion.copyWith(
-                  labelText: 'email',
-                  prefixIcon: const Icon(Icons.email),
-                  hintText: "steph@gmail.com",
-                ),
-                onChanged: (value) {
-                  email = value;
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "*required field";
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  //reset password with provider and firebase
-                  if (_formkeyGlobal.currentState!.validate()) {
-                    Provider.of<ResetpasswordAuth>(
-                      context,
-                      listen: false,
-                    ).resetPassword(email).whenComplete(() {
-                      //navigate to the login screen
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                          builder: (BuildContext context) =>  const LoginPage(),
-                        ),
-                        (route) => false,
-                      );
-                    });
-                  }
-                },
-                child: const Text(
-                  'Reset',
-                  style: kloginbutton,
-                ),
-              ),
-            ],
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+              'https://chiefexecutive.net/wp-content/uploads/2020/11/AdobeStock_246230613.jpg',
+            ),
+            fit: BoxFit.cover, // -> 02
           ),
-          key: _formkeyGlobal,
+        ),
+        child: Center(
+          child: SizedBox(
+            width: 500,
+            child: Card(
+              child: Form(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const AnimatedTxt(),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: ktextFieldDEcoratiion.copyWith(
+                          labelText: 'email',
+                          prefixIcon: const Icon(Icons.email),
+                          hintText: "steph@gmail.com",
+                        ),
+                        onChanged: (value) {
+                          email = value;
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "*required field";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          //reset password with provider and firebase
+                          if (_formkeyGlobal.currentState!.validate()) {
+                            Provider.of<ResetpasswordAuth>(
+                              context,
+                              listen: false,
+                            ).resetPassword(email).whenComplete(() {
+                              //navigate to the login screen
+                              Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      const LoginPage(),
+                                ),
+                                (route) => false,
+                              );
+                            });
+                          }
+                        },
+                        child: const Text(
+                          'Reset',
+                          style: kloginbutton,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                key: _formkeyGlobal,
+              ),
+            ),
+          ),
         ),
       ),
     );
--- lib/screens/dashboard/module.dart
+++ lib/screens/dashboard/module.dart
@@ -14,81 +14,135 @@ class _ModulesState extends State<Modules> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: GridView.extent(
-          primary: false,
-          padding: const EdgeInsets.all(16),
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          maxCrossAxisExtent: 200.0,
-          children: <Widget>[
-            InkWell(
-              onTap: () {
-                //move to module one content --the user should be able to downoad
-                //this contents are connected to the database
-                Navigator.pushNamed(context, ModuleOne.id);
-              },
-              child: const Card(
-                ///padding: EdgeInsets.all(8),
-                child: Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text('Leadership & Governance', style: TextStyle(fontSize: 20)),
-                    )),
-                color: Color.fromARGB(255, 161, 172, 196),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+                'https://chiefexecutive.net/wp-content/uploads/2020/11/AdobeStock_246230613.jpg'),
+            fit: BoxFit.cover, // -> 02
+          ),
+        ),
+        child: Center(
+          child: SizedBox(
+            width: 500,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(10, 50, 10, 50),
+              child: Card(
+                child: SafeArea(
+                  child: GridView.extent(
+                    primary: false,
+                    padding: const EdgeInsets.all(16),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    maxCrossAxisExtent: 200.0,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: InkWell(
+                          hoverColor: Colors.cyan,
+                          onTap: () {
+                            //move to module one content --the user should be able to downoad
+                            //this contents are connected to the database
+                            Navigator.pushNamed(context, ModuleOne.id);
+                          },
+                          child: const Card(
+                            ///padding: EdgeInsets.all(8),
+                            child: Center(
+                                child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                                'Leadership & Governance',
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            )),
+                            color: Color.fromARGB(255, 161, 172, 196),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: InkWell(
+                          hoverColor: Colors.cyan,
+                          onTap: () {
+                            Navigator.pushNamed(context, ModuleTwo.id);
+                          },
+                          child: const Card(
+                            //padding: const EdgeInsets.all(8),
+                            child: Center(
+                                child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                                'Strategic Human Resource Management',
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            )),
+                            color: Color.fromARGB(255, 98, 162, 214),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: InkWell(
+                          hoverColor: Colors.cyan,
+                          onTap: () {},
+                          child: const Card(
+                            //padding: const EdgeInsets.all(8),
+                            child: Center(
+                                child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                                'Project management & Strategic Planning',
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            )),
+                            color: Color.fromARGB(255, 98, 162, 214),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: InkWell(
+                          hoverColor: Colors.cyan,
+                          onTap: () {},
+                          child: const Card(
+                            //padding: const EdgeInsets.all(8),
+                            child: Center(
+                                child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                                'Financial Management',
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            )),
+                            color: Color.fromARGB(255, 161, 172, 196),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: InkWell(
+                          hoverColor: Colors.cyan,
+                          onTap: () {},
+                          child: const Card(
+                            //padding: const EdgeInsets.all(8),
+                            child: Center(
+                                child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                                'Digital teaching and learning',
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            )),
+                            color: Color.fromARGB(255, 161, 172, 196),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-            InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, ModuleTwo.id);
-              },
-              child: const Card(
-                //padding: const EdgeInsets.all(8),
-                child: Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text('Strategic Human Resource Management', style: TextStyle(fontSize: 20)),
-                    )),
-                color: Color.fromARGB(255, 98, 162, 214),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: const Card(
-                //padding: const EdgeInsets.all(8),
-                child: Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text('Project management & Strategic Planning', style: TextStyle(fontSize: 20)),
-                    )),
-                color: Color.fromARGB(255, 98, 162, 214),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: const Card(
-                //padding: const EdgeInsets.all(8),
-                child: Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text('Financial Mnagement', style: TextStyle(fontSize: 20)),
-                    )),
-                color: Color.fromARGB(255, 161, 172, 196),
-              ),
-            ),
-            InkWell(
-              onTap: () {},
-              child: const Card(
-                //padding: const EdgeInsets.all(8),
-                child: Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text('Digital teaching and learning', style: TextStyle(fontSize: 20)),
-                    )),
-                color: Color.fromARGB(255, 161, 172, 196),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
