a2c4836b3e00d935bfba9dfc2dac515738063619
flavian-anselmo <anselmoflavian4@gmail.com>
2022-01-20 18:52:44 +0300

    update

--- lib/screens/modules/Assignments/admin_view_assignmnts.dart
+++ lib/screens/modules/Assignments/admin_view_assignmnts.dart
@@ -33,41 +33,50 @@ class _AdminViewAssignmentsState extends State<AdminViewAssignments> {
       appBar: AppBar(
         title: const Text("Submitted Assignments"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Center(
-          child: SizedBox(
-            width: 600,
-            child: Card(
-              child: FutureBuilder(
-                future: fetchFromDb(),
-                //initialData: InitialData,
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    return const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: ModuleWidget(),
-                    );
-                  } else if (snapshot.connectionState ==
-                      ConnectionState.waiting) {
-                    //EasyLoading.showError("status");
-                    EasyLoading.show(status: "Loading...");
+      body: Container(
+            decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+                'https://chiefexecutive.net/wp-content/uploads/2020/11/AdobeStock_246230613.jpg'),
+            fit: BoxFit.cover, // -> 02
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Center(
+            child: SizedBox(
+              width: 600,
+              child: Card(
+                child: FutureBuilder(
+                  future: fetchFromDb(),
+                  //initialData: InitialData,
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: ModuleWidget(),
+                      );
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      //EasyLoading.showError("status");
+                      EasyLoading.show(status: "Loading...");
+                      return const Center(
+                        child: Text("Loading..."),
+                      );
+                    } else if (snapshot.hasError) {
+                      EasyLoading.showError("Error occurred while fetching!");
+                      return const Center(
+                        child: Text(
+                          "Took long to load, kindly check internet connection",
+                        ),
+                      );
+                    }
+                    EasyLoading.showInfo("No content found!");
                     return const Center(
-                      child: Text("Loading..."),
+                      child: Text("No content Found!"),
                     );
-                  } else if (snapshot.hasError) {
-                    EasyLoading.showError("Error occurred while fetching!");
-                    return const Center(
-                      child: Text(
-                        "Took long to load, kindly check internet connection",
-                      ),
-                    );
-                  }
-                  EasyLoading.showInfo("No content found!");
-                  return const Center(
-                    child: Text("No content Found!"),
-                  );
-                },
+                  },
+                ),
               ),
             ),
           ),
